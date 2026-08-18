2dcc9afac850dd8fd8824e8ac71c168c584b7eeb
venti1112 <3788365896@qq.com>
2026-08-18 23:01:52 +0800

    清空分支

--- lib/instance/download_info.dart
+++
@@ -1,7 +0,0 @@
-class DownloadInfo {
-  const DownloadInfo({required this.url, this.sha1, this.sha256});
-
-  final String url;
-  final String? sha1;
-  final String? sha256;
-}
--- lib/theme/precipitation_effect_mode.dart
+++
@@ -1,1 +0,0 @@
-enum PrecipitationEffectMode { snow, rain, hail }
